acf1123302447859ccc5e6ebda985626037ea84f
majic1985 <majic1985.mk@gmail.com>
2019-08-28 21:44:51 +0200

    Kodilla 19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,12 +11,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
-    public List<TaskDto> getTasks(){
+    public List<TaskDto> getTask(){
         return new ArrayList<>();
     }
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId){
-        return new TaskDto(1L,"test title", "test_content");
+        return new TaskDto(1L,"Test title", "Test_Content");
     }
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId){
